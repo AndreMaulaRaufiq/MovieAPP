be978e53d4582efefa9e7386d8954b70d7c9c213
AndreMaulaRaufiq <andremr107@gmail.com>
2023-11-30 20:19:24 +0700

    finished?

--- core/src/main/java/com/submissions/core/di/CoreModule.kt
+++ core/src/main/java/com/submissions/core/di/CoreModule.kt
@@ -7,6 +7,7 @@ import com.submissions.core.data.source.remote.RemoteDataSource
 import com.submissions.core.data.source.remote.network.ApiService
 import com.submissions.core.domain.repository.IMovieRepository
 import com.submissions.core.utils.AppExecutors
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -27,10 +28,18 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/5VLcahb6x4EvvFrCF2TePZulWqrLHS2jCg9Ywv6JHog=")
+            .add(hostname, "sha256/vxRon/El5KuI4vx5ey1DgmsYmRY0nDd5Cg4GfJ8S+bg=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
